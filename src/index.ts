052801b1a0a7d99f12523b3ed792ba651dfe6aff
Johannes Filter <hi@jfilter.de>
2021-05-13 20:01:47 +0200

    export types directly

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './extractors/parsePlaylistPage';
 export * from './extractors/parseSearchHistoryPage';
 export * from './extractors/parseSubscribedChannelsPage';
 export * from './extractors/parseWatchHistoryPage';
+export * from './types';
