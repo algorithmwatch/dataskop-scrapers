1378a44c649feb565c7941bcdcd662797298ed92
Johannes Filter <hi@jfilter.de>
2022-07-29 19:47:47 +0200

    Add second package to use it as an CLI

---
+++ schaufel-bin/index.js
@@ -0,0 +1,18 @@
+/*
+$ npm start ~/Downloads/data/000.json
+*/
+
+const fs = require('fs');
+const process = require('process');
+const schaufel = require('@algorithmwatch/schaufel');
+
+for (const p of process.argv.slice(2)) {
+  console.log(p);
+  let rawdata = fs.readFileSync(p);
+  let inputData = JSON.parse(rawdata);
+  const result = schaufel.redactTiktokDump(inputData);
+  fs.writeFileSync(
+    p.replace('.json', '_redacted.json'),
+    JSON.stringify(result),
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export * from './redaction';
-export * from './types';
+// export * from './types';
