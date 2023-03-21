d4ddb56ed99e34f12603c837ce554586667cd1b8
Johannes Filter <hi@jfilter.de>
2023-03-21 17:00:59 +0100

    Prepare package for monorepo

--- global.d.ts
+++
@@ -1,6 +0,0 @@
-// URLSearchParams is available globally in Node v10+ so we don't have to import it from node's lib.
-// This makes this code run in a browser environment.
-declare global {
-  const URLSearchParams: any;
-}
-export {};
--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  testEnvironment: 'node',
-  testRegex: './test/.*\\.(test|spec)?\\.(ts|ts)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/test'],
-};
